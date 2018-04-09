a896b709ed9870ce5f2bed901401c08326f5a6ea
noplan1989 <lawson.and.7.11@gmail.com>
2018-04-10 01:42:11 +0900

    vue-analyticsの追加

--- src/main.js
+++ src/main.js
@@ -1,8 +1,14 @@
 import 'babel-polyfill'
 import Vue from 'vue'
+import VueAnalytics from 'vue-analytics'
 import App from './components/App'
 import router from './router'
 import './assets/scss/index.scss'
+
+Vue.use(VueAnalytics, {
+  id: 'UA-53153991-13',
+  router
+})
 
 new Vue({
   el: '#app',
