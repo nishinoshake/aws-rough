ef263f6d8fa89943bb43ad06b378447ce867255c
noplan1989 <lawson.and.7.11@gmail.com>
2018-04-12 02:33:48 +0900

    fastclick追加

--- src/main.js
+++ src/main.js
@@ -2,22 +2,27 @@ import 'babel-polyfill'
 import Vue from 'vue'
 import VueAnalytics from 'vue-analytics'
 import SweetScroll from 'sweet-scroll'
+import FastClick from 'fastclick'
 import App from './components/App'
 import router from './router'
 import './assets/scss/index.scss'
 
-Vue.use(VueAnalytics, {
-  id: 'UA-53153991-13',
-  router
-})
+document.addEventListener('DOMContentLoaded', () => {
+  Vue.use(VueAnalytics, {
+    id: 'UA-53153991-13',
+    router
+  })
 
-new Vue({
-  el: '#app',
-  router,
-  components: { App },
-  template: '<App/>'
-})
+  new Vue({
+    el: '#app',
+    router,
+    components: { App },
+    template: '<App/>'
+  })
 
-new SweetScroll({
-  trigger: "a[href^='#']"
+  new SweetScroll({
+    trigger: "a[href^='#']"
+  })
+
+  FastClick.attach(document.body)
 })
