05168098e4113de2b1a9f9bce55c35a46fcb2e76
noplan1989 <lawson.and.7.11@gmail.com>
2018-04-14 20:36:10 +0900

    やっぱり合計は合計だ

--- src/components/cart/CartIndex.vue
+++ src/components/cart/CartIndex.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="cart">
     <section class="cart-total">
-      <h2 class="cart-total-title">月額</h2>
+      <h2 class="cart-total-title">合計</h2>
       <ServicePartsPrice :price="total" mod="large" />
     </section>
     <router-link to="detail" class="cart-detail">内訳</router-link>
