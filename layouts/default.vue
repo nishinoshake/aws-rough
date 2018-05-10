827c5e808ac6bcffa5ea1fcc10850fbad8e81610
noplan1989 <lawson.and.7.11@gmail.com>
2018-05-11 00:44:01 +0900

    レイアウトの調整

---
+++ layouts/default.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="app">
+    <div class="container">
+      <LayoutMenuPC />
+      <LayoutMenuSP />
+      <div class="contents">
+        <nuxt/>
+        <LayoutFooter />
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import LayoutMenuPC from '@/layouts/LayoutMenuPC'
+import LayoutMenuSP from '@/layouts/LayoutMenuSP'
+import LayoutFooter from '@/layouts/LayoutFooter'
+import CartIndex from '@/components/cart/CartIndex'
+import ErrorModal from '@/components/error/ErrorModal'
+import '@/assets/scss/index.scss'
+
+export default {
+  components: {
+    LayoutMenuPC,
+    LayoutMenuSP,
+    LayoutFooter,
+    CartIndex,
+    ErrorModal
+  },
+  beforeMount() {
+    console.log('hoge')
+  }
+}
+</script>
