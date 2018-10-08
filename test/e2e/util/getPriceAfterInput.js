aba3915cdf72114e8f4d1dbc10bab22a31469a29
noplan1989 <lawson.and.7.11@gmail.com>
2018-10-09 02:56:14 +0900

    getPriceAfterInputの冗長な記述をシンプルに

--- test/e2e/util/getPriceAfterInput.js
+++ test/e2e/util/getPriceAfterInput.js
@@ -1,14 +1,14 @@
-export default async function(pageInstance, url, useCase) {
-  await pageInstance.goto(url)
-  await pageInstance.waitForSelector(useCase.waitFor)
+export default async function(page, url, useCase) {
+  await page.goto(url)
+  await page.waitForSelector(useCase.waitFor)
 
-  if (useCase.actions && useCase.actions.length) {
+  if (Array.isArray(useCase.actions) && useCase.actions.length) {
     for (const action of useCase.actions) {
-      await pageInstance[action.type](action.target, action.value)
+      await page[action.type](action.target, action.value)
     }
   }
 
-  const price = await pageInstance.$eval(useCase.price.target, el => el.textContent)
+  const price = await page.$eval(useCase.price.target, el => el.textContent)
 
   return parseFloat(price.replace(/,/g, ''))
 }
