3525dd75dc6332a6d4ac1a693ee7bcf4d960b215
noplan1989 <lawson.and.7.11@gmail.com>
2019-04-09 00:50:20 +0900

    404をスパイダーマン

--- pages/error/404.vue
+++ pages/error/404.vue
@@ -1,7 +1,12 @@
 <template>
   <section class="error-notfound">
     <h1 class="error-title title-page">Not Found</h1>
-    <p class="error-text">ソンナページハ、<br>ソンザイシマセン。<br>モシカシテ、<ExternalLink href="http://kaela-web.com">キムラカエラ</ExternalLink>？</p>
+    <div class="detail-body">
+      <p class="detail-empty is-visible">
+        <span>With Great Power</span>
+        <span>Comes Great Responsibility</span>
+      </p>
+    </div>
   </section>
 </template>
 
