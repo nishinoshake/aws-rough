4795beb374eaf3bb11fa12cf687027e2c0a5a46d
noplan1989 <lawson.and.7.11@gmail.com>
2019-05-13 02:35:09 +0900

    タイトルを短くしすぎた

--- config/meta.js
+++ config/meta.js
@@ -1,6 +1,9 @@
 const SITE_TITLE = 'ざっくりAWS'
 const SITE_DESCRIPTION =
-  'AWSの料金の目安を日本円で「ざっくり」計算できるサイトです。公式のシミュレーターよりも少ない入力で手軽に概算を算出できます。料金の厳密さよりも手軽さを優先しているので、正確な料金が知りたい方は公式のシミュレーターをお使いください。'
+  'ざっくりAWSは、AWSの料金の目安を日本円で「ざっくり」計算できるサイトです。公式のシミュレーターよりも少ない入力で手軽に概算を算出できます。'
+
+const pageTitle = page => `${page}の料金を日本円で計算 | ${SITE_TITLE}`
+const pageDescription = description => `${description}${SITE_DESCRIPTION}`
 
 const meta = {
   index: {
@@ -15,62 +18,74 @@ const meta = {
     title: 'ざっくりAWSについて',
     description: SITE_DESCRIPTION
   },
+  detail: {
+    title: '料金内訳',
+    description: 'ざっくりAWSの料金内訳を確認できます。'
+  },
   ec2: {
-    title: 'EC2の料金を日本円でざっくり',
-    description: 'EC2の料金は、主にインスタンスのスペックや利用する台数、ストレージのサイズやデータ転送量で決まります。'
+    title: pageTitle('EC2'),
+    description: pageDescription(
+      'EC2の料金は、主にインスタンスのスペックや利用する台数、ストレージのサイズやデータ転送量で決まります。'
+    )
   },
   fargate: {
-    title: 'Fargateの料金を日本円でざっくり',
-    description: 'Fargateの料金は、タスク数と稼働時間、タスクに割り当てたリソースと外部へのデータ転送量で決まります。'
+    title: pageTitle('Fargate'),
+    description: pageDescription(
+      'Fargateの料金は、タスク数と稼働時間、タスクに割り当てたリソースと外部へのデータ転送量で決まります。'
+    )
   },
   lambda: {
-    title: 'Lambdaの料金を日本円でざっくり',
-    description: 'Lambdaの料金は、リクエスト数とメモリ使用量で料金が決まりますが、かなり寛大な無料枠があります。'
+    title: pageTitle('Lambda'),
+    description: pageDescription(
+      'Lambdaの料金は、リクエスト数とメモリ使用量で料金が決まりますが、かなり寛大な無料枠があります。'
+    )
   },
   elb: {
-    title: 'ELBの料金を日本円でざっくり',
+    title: pageTitle('ELB'),
     description:
       'CLBの料金は、主に利用する台数とデータ転送量で決まります。ALB/NLBの場合は少し複雑で、Load Balancer Capacity Unit(LCU)というものを考慮する必要があります。'
   },
   vpc: {
-    title: 'VPCの料金を日本円でざっくり',
-    description: 'VPCの利用は基本的に無料ですが、NAT Gatewayなどを使う場合は料金が発生します。'
+    title: pageTitle('VPC'),
+    description: pageDescription('VPCの利用は基本的に無料ですが、NAT Gatewayなどを使う場合は料金が発生します。')
   },
   route53: {
-    title: 'Route53の料金を日本円でざっくり',
-    description: 'Route53の料金は、主にホストゾーン数とクエリ数で決まります。'
+    title: pageTitle('Route53'),
+    description: pageDescription('Route53の料金は、主にホストゾーン数とクエリ数で決まります。')
   },
   apigateway: {
-    title: 'API Gatewayの料金を日本円でざっくり',
-    description: 'API Gatewayの料金は、主にリクエスト数とデータ転送量で決まります。'
+    title: pageTitle('API Gateway'),
+    description: pageDescription('API Gatewayの料金は、主にリクエスト数とデータ転送量で決まります。')
   },
   cloudfront: {
-    title: 'CloudFrontの料金を日本円でざっくり',
-    description: 'CloudFrontの料金は、主にインターネットへのデータ転送量とリクエスト数で決まります。'
+    title: pageTitle('CloudFront'),
+    description: pageDescription('CloudFrontの料金は、主にインターネットへのデータ転送量とリクエスト数で決まります。')
   },
   s3: {
-    title: 'S3の料金を日本円でざっくり',
-    description: 'S3の料金は、主にストレージの容量とデータ転送量、読み書きの回数で決まります。'
+    title: pageTitle('S3'),
+    description: pageDescription('S3の料金は、主にストレージの容量とデータ転送量、読み書きの回数で決まります。')
   },
   rds: {
-    title: 'RDSの料金を日本円でざっくり',
-    description: 'RDSの料金は、主にインスタンスのスペックや利用する台数、冗長構成、ストレージの容量で決まります。'
+    title: pageTitle('RDS'),
+    description: pageDescription(
+      'RDSの料金は、主にインスタンスのスペックや利用する台数、冗長構成、ストレージの容量で決まります。'
+    )
   },
   aurora: {
-    title: 'Auroraの料金を日本円でざっくり',
-    description: 'Auroraの料金は、主にインスタンスのスペックや、レプリカの数、ストレージの容量で決まります。'
+    title: pageTitle('Aurora'),
+    description: pageDescription(
+      'Auroraの料金は、主にインスタンスのスペックや、レプリカの数、ストレージの容量で決まります。'
+    )
   },
   dynamodb: {
-    title: 'DynamoDBの料金を日本円でざっくり',
-    description: 'DynamoDBの料金は、主にストレージの容量と読み込み/書き込みキャパシティユニットで決まります。'
+    title: pageTitle('DynamoDB'),
+    description: pageDescription(
+      'DynamoDBの料金は、主にストレージの容量と読み込み/書き込みキャパシティユニットで決まります。'
+    )
   },
   elasticache: {
-    title: 'ElastiCacheの料金を日本円でざっくり',
-    description: 'ElastiCacheの料金は、主にノードタイプと台数で決まります。'
-  },
-  detail: {
-    title: '料金内訳',
-    description: 'ざっくりAWSの料金内訳を確認できます。'
+    title: pageTitle('ElastiCache'),
+    description: pageDescription('ElastiCacheの料金は、主にノードタイプと台数で決まります。')
   }
 }
 
