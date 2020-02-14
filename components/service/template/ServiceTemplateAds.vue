7fab1549789f790d335cf2074d5e77014a09e551
Tatsuya Kinno <lawson.and.7.11@gmail.com>
2020-02-14 09:47:10 +0900

    広告の文言長すぎ

--- components/service/template/ServiceTemplateAds.vue
+++ components/service/template/ServiceTemplateAds.vue
@@ -4,9 +4,9 @@
       <p class="title-daitan"><span>広告</span></p>
       <div class="service-ad">
         <p>
-          このエリアには<ExternalLink href="https://www.google.com/intl/ja_jp/adsense/start/"
+          <ExternalLink href="https://www.google.com/intl/ja_jp/adsense/start/"
             >Googleアドセンス</ExternalLink
-          >の広告が表示されます。興味がある広告が表示されたときに、クリックして内容をご確認いただくと、サイト運営者の収益になります。そのお金で<ExternalLink
+          >の広告です。<br />収益で<ExternalLink
             href="https://www.redbull.com/jp-ja/energydrink"
             >Red Bull</ExternalLink
           >を買います。
