ce11802f9c5d956892b6cdc7e8fe26cb662476f6
Tatsuya Kinno <kinnou@mefilas.com>
2020-05-19 22:05:43 +0900

    公式ツールが先

--- layouts/LayoutFooter.vue
+++ layouts/LayoutFooter.vue
@@ -2,10 +2,10 @@
   <footer class="footer">
     <ul class="footer-list">
       <li class="footer-item">
-        <ExternalLink href="https://github.com/nishinoshake/aws-rough">GitHub</ExternalLink>
+        <ExternalLink href="https://calculator.s3.amazonaws.com/index.html?lng=ja_JP">公式ツール</ExternalLink>
       </li>
       <li class="footer-item">
-        <ExternalLink href="https://calculator.s3.amazonaws.com/index.html?lng=ja_JP">公式ツール</ExternalLink>
+        <ExternalLink href="https://github.com/nishinoshake/aws-rough">GitHub</ExternalLink>
       </li>
       <li class="footer-item">
         <ExternalLink href="https://hatebu.noplan.cc">先週のはてブ</ExternalLink>
