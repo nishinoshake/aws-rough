f3c6dc4b1952dab8b8e220def8b1b16eb72af967
Tatsuya Kinno <kinnou@mefilas.com>
2020-05-26 10:33:45 +0900

    必要なときに内訳表示

--- layouts/LayoutCart.vue
+++ layouts/LayoutCart.vue
@@ -1,11 +1,13 @@
 <template>
-  <nuxt-link to="/detail/" class="cart">
-    <div class="cart-total">
-      <span class="cart-label">合計</span>
-      <ServicePartsPrice :price="total" mod="large" />
-    </div>
-    <span class="cart-link">内訳</span>
-  </nuxt-link>
+  <transition name="cart">
+    <nuxt-link v-if="total" to="/detail/" class="cart">
+      <div class="cart-total">
+        <span class="cart-label">合計</span>
+        <ServicePartsPrice :price="total" mod="large" />
+      </div>
+      <span class="cart-link">内訳</span>
+    </nuxt-link>
+  </transition>
 </template>
 
 <script>
