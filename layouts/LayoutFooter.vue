9d0bdf8706c8dc5b57e32f4d3a85d0d2b0eb93d3
Tatsuya Kinno <kinnou@mefilas.com>
2020-06-12 02:03:29 +0900

    クレジット

--- layouts/LayoutFooter.vue
+++ layouts/LayoutFooter.vue
@@ -1,16 +1,20 @@
 <template>
   <footer class="footer" id="footer">
     <div class="footer-container">
-      <p class="footer-label">&lt;footer&gt;</p>
+      <p class="footer-label">
+        <ExternalLink href="https://noplan.cc">noplan.cc</ExternalLink>
+      </p>
     </div>
   </footer>
 </template>
 
 <script>
+import ExternalLink from '@/components/text/ExternalLink'
 import serviceConfig from '@/config/service/mokuji'
 
 export default {
   name: 'LayoutFooter',
+  components: { ExternalLink },
   data() {
     return {
       services: serviceConfig
