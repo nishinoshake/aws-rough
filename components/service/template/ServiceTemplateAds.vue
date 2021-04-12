4c92c2ebf29a27e2c83275f1e9f50269076d9bbc
Tatsuya Kinno <kinnou@mefilas.com>
2021-04-13 04:19:41 +0900

    これは広告です

--- components/service/template/ServiceTemplateAds.vue
+++ components/service/template/ServiceTemplateAds.vue
@@ -2,11 +2,11 @@
   <div class="service-ad">
     <div class="service-ad-inner">
       <section class="section">
-        <h2 class="title-section"><span>広告</span></h2>
         <div class="service-content">
           <no-ssr>
             <Adsense data-ad-client="ca-pub-7912727364342998"></Adsense>
           </no-ssr>
+          <p class="service-ad-label">※これは広告です</p>
         </div>
       </section>
     </div>
