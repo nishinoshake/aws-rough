e84a51810ce1c5139ac93f2213110081d1c2203e
Tatsuya Kinno <kinnou@mefilas.com>
2021-04-13 00:14:05 +0900

    広告ヘッダー固定

--- components/service/template/ServiceTemplateAds.vue
+++ components/service/template/ServiceTemplateAds.vue
@@ -1,13 +1,15 @@
 <template>
   <div class="service-ad">
-    <section class="section">
-      <h2 class="title-section"><span>広告</span></h2>
-      <div class="service-content">
-        <no-ssr>
-          <Adsense data-ad-client="ca-pub-7912727364342998"></Adsense>
-        </no-ssr>
-      </div>
-    </section>
+    <div class="service-ad-inner">
+      <section class="section">
+        <h2 class="title-section"><span>広告</span></h2>
+        <div class="service-content">
+          <no-ssr>
+            <Adsense data-ad-client="ca-pub-7912727364342998"></Adsense>
+          </no-ssr>
+        </div>
+      </section>
+    </div>
   </div>
 </template>
 
